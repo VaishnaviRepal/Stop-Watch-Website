5e6dad8313a76c946f677f4c567aaa08a961bdd3
VaishnaviRepal <vaishnavi.repal7873@gmail.com>
2026-09-24 13:56:02 +0530

    Delete component added to filter

--- clock-watch-website/src/CustomHooks/useTodos.tsx
+++ clock-watch-website/src/CustomHooks/useTodos.tsx
@@ -4,10 +4,15 @@
 import {useState, useEffect} from 'react';
 import axios, { type AxiosResponse } from 'axios';
 
+export type TodoType = {
+    id : number,
+    title : string,
+    // setTodos : any
+}
 //Resusale func, easy to debug n modify n implement
 export function useTodos(){
 
-    const [todos, setTodos ] = useState([]);
+    const [todos, setTodos] = useState<TodoType[]>([]);
 
     useEffect(()=>{
         axios.get("https://jsonplaceholder.typicode.com/todos")
@@ -16,19 +21,19 @@ export function useTodos(){
             })
 
         //to keep refreshin / updating every 10 s new todos
-        const intervalId  = setInterval(()=>{
-            axios.get("https://jsonplaceholder.typicode.com/todos")
-                .then((response: AxiosResponse)=>{
-                    setTodos(response.data);
-                })
-            }, 10 * 1000);
+        // const intervalId  = setInterval(()=>{
+        //     axios.get("https://jsonplaceholder.typicode.com/todos")
+        //         .then((response: AxiosResponse)=>{
+        //             setTodos(response.data);
+        //         })
+        //     }, 10 * 1000);
 
-        return(()=>{
-            clearInterval(intervalId);
-        })  
+        // return(()=>{
+        //     clearInterval(intervalId);
+        // })  
     }, []);
 
     
-    return todos;
+    return [todos, setTodos] as const;
 }
 
--- clock-watch-website/src/CustomHooks_Study.tsx
+++ clock-watch-website/src/CustomHooks_Study.tsx
@@ -1,27 +1,80 @@
 //###---Applying the custom hook made -> useTodos
 
 //Import the custom hook made for this purpose esp
-import {useTodos} from "./CustomHooks/useTodos";
+import {useTodos, type TodoType} from "./CustomHooks/useTodos";
+
+//To get and print the todos 
+// function CustomHooks_Study(){
+//     const todos = useTodos(); // get those todos
+
+//     return (
+//         <div>
+//             {(todos as { title: string }[]).map(t => 
+//             <div
+//                 style = {{
+//                     padding : "20px",
+//                     border:"2px solid black",
+//                     margin : "20px",
+
+//                 }}
+//                 >
+//                 {t.title}
+//             </div>)}
+//         </div>
+//     );
+// }
+
+// export default CustomHooks_Study;
 
 
+
+//To Delete the todos printed
 function CustomHooks_Study(){
-    const todos = useTodos(); // get those todos
+
+    // Pass the type to useState so it's not inferred as never[]
+    const [todos, setTodos] = useTodos();
+    // as  [Todo[], React.Dispatch<React.SetStateAction<Todo[]>>]; // get those todos, we need setTodos too for deletion purpose
 
     return (
         <div>
-            {(todos as { title: string }[]).map(t => 
-            <div
-                style = {{
+            {todos.map(t => 
+                <Todos
+                //Calls a component for deletion and updation while passing the foll componenets
+                    key = {t.id}
+                    title = {t.title}
+                    id  = {t.id}
+                    setTodos = {setTodos}
+                />
+            )}
+                      
+        </div>
+    )
+}
+
+function Todos({id , title , setTodos} : TodoType & { setTodos: React.Dispatch<React.SetStateAction<TodoType[]>> }){
+    //On page return the title and delete button -> on click deletes/ removes it on page by filter
+    return (
+        <div
+            style = {{
                     padding : "20px",
                     border:"2px solid black",
-                    margin : "20px",
-
+                    margin : "20px"
                 }}
-                >
-                {t.title}
-            </div>)}
+        >
+            <div>{title}    </div>
+
+            <button
+                onClick = {
+                    ()=>{
+                        setTodos( todos => todos.filter(x => x.id !== id) )
+                    }
+                }
+            >Delete</button>
         </div>
-    );
+    )
+    
+    
+
 }
 
 export default CustomHooks_Study;
